1feeccdda5d3c9a627357fa8010a1231173a98c8
Shiva Rudraraju <rudraa@umich.edu>
2015-03-12 01:48:12 -0400

    testing....

--- applications/mechanochemo/flux/main.c
+++ applications/mechanochemo/flux/main.c
@@ -9,23 +9,27 @@ extern "C" {
 #define ADSacado
 #define numVars 27
 //physical parameters
-#define C4 1.0
-#define C3 1.0
-#define C2 1.0
-#define E4 1.0*(c)
-#define E3 1.0*(c)
-#define E2 1.0*(c)
-#define E4_c 1.0*(c)
-#define E3_c 1.0*(c)
-#define E2_c 1.0*(c)
-#define E4_cc 1.0*(c)
-#define E3_cc 1.0*(c)
-#define E2_cc 1.0*(c)
-#define Eii 1.0
-#define Eij 1.0
-#define Cl 1.0 //Clambda - constant for gradC.gradC
-#define El 1.0 //ELambda - constant for gradE.gradE
-#define Gl 0.0 //Glambda - constant for abs(gradC.gradE)
+#define Cs 1.0
+#define Cd -2.0
+#define C4 (-16*Cd/std::pow(Cs,4))
+#define C3 (32*Cd/std::pow(Cs,3))
+#define C2 (-16*Cd/std::pow(Cs,2))
+#define Es 1.0
+#define Ed -1.0
+#define E4 (-Ed/std::pow(Es,4))
+#define E3 0.0
+#define E2 (2*Ed/std::pow(Es,2))*((2*c-Cs)/Cs)
+#define E4_c 0.0
+#define E3_c 0.0
+#define E2_c (2*Ed/std::pow(Es,2))*(2.0/Cs)
+#define E4_cc 0.0
+#define E3_cc 0.0
+#define E2_cc 0.0
+#define Eii (-2*Ed/std::pow(Es,2))
+#define Eij (-2*Ed/std::pow(Es,2))
+#define Cl 0.01 //Clambda - constant for gradC.gradC
+#define El 0.01 //ELambda - constant for gradE.gradE
+#define Gl 0.0  //Glambda - constant for abs(gradC.gradE)
 //stress
 #define PiJ (2*Eii*e1*e1_FiJ + 2*Eij*e6*e6_FiJ + (4*E4*e2*e2*e2 + 3*E3*e2*e2 + 2*E2*e2)*e2_FiJ + (El*e2_1+Gl*c_1/2)*e2_1_FiJ + (El*e2_2+Gl*c_2/2)*e2_2_FiJ)
 #define BetaiJK ((El*e2_1+Gl*c_1/2)*e2_1_FiJK + (El*e2_2+Gl*c_2/2)*e2_2_FiJK)
@@ -36,16 +40,15 @@ extern "C" {
 #define bcVAL 1
 #define FLUX 0
 #define flux 1.0
-#define uDirichlet 0.01
+#define uDirichlet 0.001
 //other variables
 #define NVal 100
-#define Es 0.01
-#define DVal 1.0
-#define CVal 1.0
+#define DVal 0.1
+#define CVal 5.0
 #define gamma 1.0
-#define cbar 0.05
+#define cbar 0.01
 //time stepping
-#define dtVal 1.0e-7
+#define dtVal 1.0e-5
 
 //other problem headers
 #include "../../../include/appctx.h"
