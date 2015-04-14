5ed7516b71f2c44b35e130e90b06efd477cb31d5
greght <greght@umich.edu>
2015-04-14 09:23:56 -0400

    Added applications folder for configurational forces.

---
+++ applications/configurationalForces/3D/main.c
@@ -0,0 +1,139 @@
+#include <math.h> 
+extern "C" {
+#include "petiga.h"
+}
+
+//model parameters
+#define DIM 3
+#define GridScale 1.0
+#define ADSacado //enables Sacado for AD instead of Adept
+#define numVars 81 //81 in 3D, 18 in 2D for DIM dof
+
+//generic headers
+#include "../../../include/fields.h"
+#include "../../../include/appctx.h"
+#include "../../../include/evaluators.h"
+#include "../../../include/init.h"
+
+//physical parameters
+#define Es 1.0e-2
+#define Ed -1.0
+#define E4 (-3*Ed/(2*std::pow(Es,4)))
+#define E3 (-Ed/(std::pow(Es,3)))*(c)
+#define E2 (3*Ed/(2*std::pow(Es,2)))*(2*c-1.0)
+#define Eii (-1.5*Ed/std::pow(Es,2))
+#define Eij (-1.5*Ed/std::pow(Es,2))
+#define El 0.1 //**ELambda - constant for gradE.gradE
+//material model (stress expressions)
+//non-gradient St-Venant Kirchoff model with lambda=mu=1
+//#define PiJ ((E[0][0]+E[1][1]+E[2][2])*F[i][J] + 2*(F[i][0]*E[0][J]+F[i][1]*E[1][J]+F[i][2]*E[2][J]))
+//#define BetaiJK (0.0)
+//gradient model
+#define PiJ (2*Eii*e1*e1_FiJ + 2*Eij*e4*e4_FiJ + 2*Eij*e5*e5_FiJ + 2*Eij*e6*e6_FiJ + (2*E2*e2-6*E3*e2*e3+4*E4*e2*(e2*e2+e3*e3))*e2_FiJ + (2*E2*e3+3*E3*(e3*e3-e2*e2)+4*E4*e3*(e2*e2+e3*e3))*e3_FiJ + 2*El*(e2_1*e2_1_FiJ + e2_2*e2_2_FiJ + e2_3*e2_3_FiJ + e3_1*e3_1_FiJ + e3_2*e3_2_FiJ + e3_3*e3_3_FiJ))
+#define BetaiJK  2*El*(e2_1*e2_1_FiJK + e2_2*e2_2_FiJK + e2_3*e2_3_FiJK + e3_1*e3_1_FiJK + e3_2*e3_2_FiJK + e3_3*e3_3_FiJK)
+//boundary conditions
+#define bcVAL 2 //**
+#define uDirichlet 0.001
+//other variables
+#define NVal 50 //**
+//time stepping
+#define dtVal 1.0e-2 //** // used to set load parameter..so 0<dtVal<1
+#define skipOutput 1
+
+//physics headers
+#include "../../../src/mechanics/model.h"
+#include "../../../src/mechanics/initialConditions.h"
+#include "../../../src/mechanics/output.h"
+
+//snes convegence test
+PetscErrorCode SNESConvergedTest(SNES snes, PetscInt it,PetscReal xnorm, PetscReal snorm, PetscReal fnorm, SNESConvergedReason *reason, void *ctx){
+  AppCtx *user  = (AppCtx*) ctx;
+  PetscPrintf(PETSC_COMM_WORLD,"xnorm:%12.6e snorm:%12.6e fnorm:%12.6e\n",xnorm,snorm,fnorm);  
+  //custom test
+  if (NVal>=15){
+    if (it>500){
+      *reason = SNES_CONVERGED_ITS;
+      return(0);
+    }
+  }
+  //default test
+  PetscFunctionReturn(SNESConvergedDefault(snes,it,xnorm,snorm,fnorm,reason,ctx));
+}
+
+int main(int argc, char *argv[]) {
+  PetscErrorCode  ierr;
+  ierr = PetscInitialize(&argc,&argv,0,0);CHKERRQ(ierr);
+ 
+  //application context objects and parameters
+  AppCtx user;
+  user.dt=dtVal;
+  user.he=GridScale*1.0/NVal; 
+  PetscInt p=2;
+  const unsigned int DOF=DIM;
+
+  //initialize
+  IGA iga;
+  Vec U,U0;
+  user.iga = iga;
+  user.U0=&U0;
+  user.U=&U;
+
+  PetscPrintf(PETSC_COMM_WORLD,"initializing...\n");
+  init<DOF>(user, NVal, p);
+
+  //initial conditions
+  ierr = IGACreateVec(user.iga,user.U);CHKERRQ(ierr);
+  ierr = IGACreateVec(user.iga,user.U0);CHKERRQ(ierr);
+  ierr = FormInitialCondition(user.iga, *user.U0, &user); 
+  ierr = VecCopy(*user.U0, *user.U);CHKERRQ(ierr);
+
+  //Dirichlet boundary conditons for mechanics
+  PetscPrintf(PETSC_COMM_WORLD,"applying bcs...\n");
+  double dVal=uDirichlet*GridScale;
+#if bcVAL==0
+  //shear BC
+  ierr = IGASetBoundaryValue(iga,0,0,1,dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(iga,0,1,1,-dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(iga,1,0,0,dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(iga,1,1,0,-dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(iga,2,0,2,0.0);CHKERRQ(ierr);  
+#elif bcVAL==1
+  //free BC
+  ierr = IGASetBoundaryValue(user.iga,0,0,0,0.0);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,1,0,1,0.0);CHKERRQ(ierr);
+  ierr = IGASetBoundaryValue(user.iga,2,0,2,0.0);CHKERRQ(ierr);
+  ierr = IGASetBoundaryValue(user.iga,0,1,0,dVal);CHKERRQ(ierr);  
+#elif bcVAL==2
+  //fixed BC
+  ierr = IGASetBoundaryValue(user.iga,0,0,0,0.0);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,0,0,1,0.0);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,0,0,2,0.0);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,0,1,1,dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,0,1,2,dVal);CHKERRQ(ierr);  
+#endif 
+
+  //time stepping
+  TS ts;
+  ierr = IGACreateTS(user.iga,&ts);CHKERRQ(ierr);
+  ierr = TSSetType(ts,TSBEULER);CHKERRQ(ierr);
+  ierr = TSSetDuration(ts,100,1.0);CHKERRQ(ierr);
+  ierr = TSSetTime(ts,0.0);CHKERRQ(ierr);
+  ierr = TSSetTimeStep(ts,user.dt);CHKERRQ(ierr);
+  ierr = TSMonitorSet(ts,OutputMonitor<DIM>,&user,NULL);CHKERRQ(ierr);  
+  ierr = TSSetFromOptions(ts);CHKERRQ(ierr);
+
+  //set snes convergence tests
+  SNES snes;
+  ierr = TSGetSNES(ts,&snes); CHKERRQ(ierr);
+  ierr = SNESSetConvergenceTest(snes,SNESConvergedTest,&user,NULL); CHKERRQ(ierr);
+
+  //run
+  PetscPrintf(PETSC_COMM_WORLD,"running...\n");
+  user.ts=&ts;
+  ierr = TSSolve(ts,*user.U);CHKERRQ(ierr);
+
+  //finalize
+  ierr = PetscFinalize();CHKERRQ(ierr);
+  return 0;
+}
+
