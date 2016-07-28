ee52d75ec11fc883e759550208fe1745f54e8a1f
greght <greght@umich.edu>
2016-07-28 16:06:50 -0400

    Updated mechanochem 3D example to new code structure.

---
+++ initBounValProbs/mechanochemo/3D/boundaryConditions.cc
@@ -0,0 +1,41 @@
+//extern "C" {
+#include "petiga.h"
+//}
+#include "IBVPHeaders.h"
+
+int boundaryConditions(AppCtx& user, double scale){
+  PetscErrorCode  ierr;
+
+  double dVal=scale*user.uDirichlet*user.GridScale;
+  PetscPrintf(PETSC_COMM_WORLD,"  dVal: %12.6e  \n",dVal);
+	/*
+  //shear BC
+  ierr = IGASetBoundaryValue(iga,0,0,1,dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(iga,0,1,1,-dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(iga,1,0,0,dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(iga,1,1,0,-dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(iga,2,0,2,0.0);CHKERRQ(ierr);  
+
+  //free BC
+  ierr = IGASetBoundaryValue(user.iga,0,0,0,0.0);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,1,0,1,0.0);CHKERRQ(ierr);
+  ierr = IGASetBoundaryValue(user.iga,2,0,2,0.0);CHKERRQ(ierr);
+  ierr = IGASetBoundaryValue(user.iga,0,1,0,dVal);CHKERRQ(ierr); 
+ 	*/
+  //fixed BC
+  ierr = IGASetBoundaryValue(user.iga,0,0,0,0.0);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,0,0,1,0.0);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,0,0,2,0.0);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,0,1,1,dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,0,1,2,dVal);CHKERRQ(ierr); 
+	/*
+  //hydrostatic like BC
+  ierr = IGASetBoundaryValue(user.iga,0,0,0,0.0);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,1,0,1,0.0);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,2,0,2,0.0);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,0,1,0,dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,1,1,1,dVal);CHKERRQ(ierr);  
+  ierr = IGASetBoundaryValue(user.iga,2,1,2,dVal);CHKERRQ(ierr);    
+	*/
+  return 0;
+}
---
+++ initBounValProbs/mechanochemo/3D/initialConditions.h
@@ -0,0 +1,60 @@
+#ifndef initialconditions_
+#define initialconditions_
+
+typedef struct {
+  PetscReal ux, uy;
+  PetscReal c;
+#if DIM==3
+  PetscReal uz;
+#endif
+} Field;
+
+
+PetscErrorCode FormInitialCondition(IGA iga, Vec U, AppCtx *user)
+{	
+  PetscErrorCode ierr;
+  PetscFunctionBegin;
+  srand(5);
+  DM da;
+  ierr = IGACreateNodeDM(iga,DIM+1,&da);CHKERRQ(ierr);
+#if DIM==2
+  Field **u;
+#elif DIM==3
+  Field ***u;
+#endif
+  ierr = DMDAVecGetArray(da,U,&u);CHKERRQ(ierr);
+  DMDALocalInfo info;
+  ierr = DMDAGetLocalInfo(da,&info);CHKERRQ(ierr);
+#if FLUX==3 //Quench
+  double c_average=0.46;
+#else       //Flux 
+  double c_average=0.05;
+#endif
+#if DIM==2
+  PetscInt i,j;
+  for(i=info.xs;i<info.xs+info.xm;i++){
+    for(j=info.ys;j<info.ys+info.ym;j++){
+      u[j][i].ux=0.0;
+      u[j][i].uy=0.0;
+      u[j][i].c= c_average + 0.01*(0.5 - (double)(rand() % 100 )/100.0);
+    }
+  }
+#elif DIM==3
+  PetscInt i,j,k;
+  for(i=info.xs;i<info.xs+info.xm;i++){
+    for(j=info.ys;j<info.ys+info.ym;j++){
+      for(k=info.zs;k<info.zs+info.zm;k++){
+	u[k][j][i].ux=0.0;
+	u[k][j][i].uy=0.0;
+	u[k][j][i].uz=0.0;
+	u[k][j][i].c= c_average + 0.01*(0.5 - (double)(rand() % 100 )/100.0);
+      }
+    }
+  }    
+#endif
+  ierr = DMDAVecRestoreArray(da,U,&u);CHKERRQ(ierr); 
+  ierr = DMDestroy(&da);;CHKERRQ(ierr); 
+  PetscFunctionReturn(0); 
+}
+
+#endif
